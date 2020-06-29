a971ffbbc2a23b4477d60f536934549638d48344
”名前” <>
2020-06-29 11:56:43 +0000

    りんご

---
+++ ruby_projects/hello.rb
@@ -0,0 +1,42 @@
+puts "hello ruby !!!!!!"
+puts "Branding Engineer"
+puts "Hello"
+puts "HR Tech"
+
+puts 2 + 2 #足し算
+puts 2 - 2 #引き算
+puts 2 * 2 #掛け算
+puts 2 / 2 #割り算
+
+puts "1" + "1"
+puts "おはよう" + "おやすみ"
+
+date = "リンゴ"
+num = "1234"
+p date
+p date + num
+
+fruits1 = "リンゴ"
+fruits2 = "メロン"
+fruits3 = "バナナ"
+fruits4 = "イチゴ"
+
+fruits = ["リンゴ", "メロン", "バナナ", "イチゴ"]
+p fruits
+p fruits2
+array = [1,2,3,4]
+
+p array[0]
+p array[1]
+p array[2]
+p array[3]
+
+p fruits[-1]
+
+fruits = { name: 'りんご', price: 100 }
+p fruits[:name]
+p fruits[:price]
+
+p "りんごkokobjsdncuiweh".length
+p fruits.length
+p array.length
